623d10a0e25ee6d7425df3ab0c96517964e924c3
ItsAlex <me@itsalex.fr>
2017-11-24 19:15:11 +0100

    Malloc work for small and large

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 12:11:34 by malexand          #+#    #+#             */
-/*   Updated: 2017/11/24 18:59:07 by alex             ###   ########.fr       */
+/*   Updated: 2017/11/24 19:13:15 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ void			*find_block(t_block *block, int type, size_t size)
 {
 	if (block == NULL)
 		return NULL;
-	else if (block->size == 0 && block->size_left_aft >= size)
+	ft_putendl("Block != NULL");
+	if (block->size == 0 && block->size_left_aft >= size)
 	{
 		block->size = size;
 		block->page->max_size_left -= size;
@@ -56,13 +57,27 @@ void			*find_block(t_block *block, int type, size_t size)
 
 void			*find_page(t_page *page, int type, size_t size)
 {
-	if (page->max_size_left < size + sizeof(t_block))
+	if (page->block == NULL)
 	{
+		if ((page->block = (t_block *)mmap(NULL, size_alloc(size), PROT_READ | PROT_WRITE,
+								MAP_ANON | MAP_PRIVATE, -1, 0)) == NULL)
+			return NULL;
+		page->block->size_left_aft = size_alloc(size) - sizeof(t_block);
+		page->block->mem = (char*)page->block + sizeof(t_block);
+		page->block->size = size;
+		page->block->page = page;
+		page->block->next = NULL;
+		return page->block->mem;
+	}
+	else if (page->max_size_left < size + sizeof(t_block))
+	{
+		ft_putendl("Create new page");
 		if (page->next == NULL)
 			if ((page->next = new_page(size)) == NULL)
 				return NULL;
 		return find_page(page->next, type, size);
 	}
+	ft_putendl("Search for free block");
 	return find_block(page->block, type, size);
 }
 
